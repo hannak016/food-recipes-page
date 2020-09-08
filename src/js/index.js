530436b440d9b3c399e13c4d135256621cfcbe4a
Zihan Kong <zihan.kong@campus.lmu.de>
2020-09-08 11:56:03 +0200

    search input

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,35 @@
 //controller file
-import sth from './models/Search';
+import Search from './models/Search';
 import { add, multiply, ID } from './views/searchView';
-//give the same as api name, and you do not need to give the path 
-//import axios from 'axios';
+/* import axios from 'axios';
 import "core-js/stable";
-import "regenerator-runtime/runtime";
+import "regenerator-runtime/runtime"; */
+let state = {};
 
 
-//query the API
-async function getResults(query){
-    //const proxy =  'https://cors-anywhere.herokuapp.com/';
-    
-    //try{
-       // compare to fetch: it returns automatically  json;and better at error handling.
-        /* const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`); */
-       /* search.js
-        const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`); */
-        /* const recipes = res.data.recipes;
-        console.log(recipes); */
-        //console.log(res);
-    //}
-    /* catch(error){
-        alert(error);
-    }  */ 
+async function controlResults(query){
+    //const query = 'pizza';
+
+    if(query){
+        //search request
+        
+      
+        //get data 
+        state.search = new Search(query);
+        await state.search.getResults();
+
+        //prepare the ui(view)
+
+        //render the ui(view)
+        console.log(state.search);
+    } 
 }
 
-//getResults('pizza');
+
+document.querySelector(".search").addEventListener('submit',e=>{
+    e.preventDefault();
+    const query = document.querySelector(".search__field").value;
+    console.log(query);
+    controlResults(query);
+})
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,25 @@
-//const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+//give the same as api name, and you do not need to give the path (hover to check)
+import axios from 'axios';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+    //query the API
+    async getResults(){ 
+        try{
+            // compare to fetch: it returns automatically  json;and better at error handling.
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);//using ? in order to save it to url
+            this.result = res.data.recipes;
+            //console.log(this.result);    
+        }
+        catch(error){
+            alert(error);
+        } 
+    }
+}
+
